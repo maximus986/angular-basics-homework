d7a6941ae631ea17f6a2b0d86252de37594caed2
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2019-03-17 23:31:30 +0100

    Implement product component to render product list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
-import { ProductListItemComponent } from './components/product-list-item/product-list-item.component';
+import { ProductListItemComponent } from './components/products/product-list-item/product-list-item.component';
 import { ProductsComponent } from './components/products/products.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    JumbotronComponent,
-    ProductListItemComponent,
-    ProductsComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, JumbotronComponent, ProductListItemComponent, ProductsComponent, FooterComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../models/Product';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  @Input() products: Product[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.products = [
+      {
+        title: 'Backpack for men',
+        photo: 'https://colorlib.com/etc/fashe/images/item-01.jpg',
+        oldPrice: 300,
+        price: 280,
+        cartCount: 0
+      },
+      {
+        title: 'Backpack Black',
+        photo: 'https://colorlib.com/etc/fashe/images/item-02.jpg',
+        oldPrice: 0,
+        price: 190,
+        cartCount: 0
+      },
+      {
+        title: 'Spring Jacket',
+        photo: 'https://colorlib.com/etc/fashe/images/item-03.jpg',
+        oldPrice: 0,
+        price: 160,
+        cartCount: 0
+      },
+      {
+        title: 'White shoes',
+        photo: 'https://colorlib.com/etc/fashe/images/item-06.jpg',
+        oldPrice: 0,
+        price: 130,
+        cartCount: 0
+      }
+    ];
   }
-
 }
